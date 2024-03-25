6fd176613077b0cba5defbac24d87b59ebc8eb1e
Katherine Martinez <kmartinezmedia@gmail.com>
2024-03-25 16:29:09 -0400

    feat: pretty format css preview (#30)

---
+++ apps/web-demo/src/actions/format.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import prettier from 'prettier';
+
+export default async function formatCode({
+  code,
+  parser,
+}: { code: string; parser: prettier.Options['parser'] }) {
+  return prettier.format(code, {
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: 'all',
+    printWidth: 100,
+    parser,
+  });
+}
--- apps/web-demo/src/app/playground/[id]/css/page.tsx
+++ apps/web-demo/src/app/playground/[id]/css/page.tsx
@@ -1,12 +1,22 @@
 'use client';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { PlaygroundDataContext } from '../_provider';
+import formatCode from '@/actions/format';
 
 export default function Page() {
   const data = useContext(PlaygroundDataContext);
+  const [code, setCode] = useState<string>();
+  const css = data.css;
+
+  useEffect(() => {
+    formatCode({ code: css, parser: 'css' }).then((formattedCode) => {
+      setCode(formattedCode);
+    });
+  }, [css]);
+
   return (
-    <pre>
-      <code>{data.css}</code>
+    <pre style={{ textWrap: 'pretty' }}>
+      <code>{code}</code>
     </pre>
   );
 }
