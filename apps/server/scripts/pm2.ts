3aa132f2ea097d09b019ba8459ccb514fd19201e
kat <kmartinezmedia@gmail.com>
2025-07-10 00:51:42 -0400

    fix pm2 logic

--- apps/server/scripts/pm2.ts
+++ apps/server/scripts/pm2.ts
@@ -13,14 +13,11 @@ const { values } = parseArgs({
   allowPositionals: true,
 });
 
-const { PM2_APP_NAME } = Bun.env;
-const BUN_PATH = Bun.which('bun');
 const SERVER_DIRECTORY = path.resolve(__dirname, '../');
 const PM2_CONFIG = `${SERVER_DIRECTORY}/pm2.config.cjs`;
-const SERVER_ENTRYPOINT = `${SERVER_DIRECTORY}/src/index.ts`;
 
 async function runCommand(command: 'start' | 'restart') {
-  await $`pm2 ${command} --interpreter ${BUN_PATH} ${SERVER_ENTRYPOINT} --name ${PM2_APP_NAME} ${PM2_CONFIG} `;
+  await $`pm2 ${command} ${PM2_CONFIG}`;
 }
 
 if (values.restart) {
