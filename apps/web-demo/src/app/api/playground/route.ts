38cb303516a74669cced87ee6bda5ba4d227e7f7
kat <kmartinezmedia@gmail.com>
2025-10-22 21:53:54 -0400

    move bun usage to route

--- apps/web-demo/src/app/api/playground/route.ts
+++ apps/web-demo/src/app/api/playground/route.ts
@@ -1,21 +1,87 @@
+import { $ } from 'bun';
+import type { SafelistMap } from 'katcn/cli/types';
+import { convertSafelistMapToTailwindCss } from 'katcn/cli/utils/convertSafelistMapToTailwindCss';
+import { createTsMorphProject } from 'katcn/cli/utils/createTsMorphProject';
+import { processSafelistForSourceFile } from 'katcn/cli/utils/processSafelistForSourceFile';
 import { NextResponse } from 'next/server';
-import { transformPlaygroundCode } from '@/actions/transform-playground-code';
-
+import { ts } from 'ts-morph';
 import { db } from '@/lib/firebase/firebase-admin';
+
+const project = createTsMorphProject({
+  skipAddingFilesFromTsConfig: true,
+  compilerOptions: {
+    module: ts.ModuleKind.CommonJS,
+    target: ts.ScriptTarget.ES2015,
+    jsx: ts.JsxEmit.ReactJSX,
+  },
+});
+
+async function transformJs({ id, jsInput }: { id: string; jsInput: string }) {
+  const sourceFile = project.createSourceFile(`${id}.tsx`, jsInput, {
+    overwrite: true,
+  });
+
+  const safelistMap: SafelistMap = new Map();
+  processSafelistForSourceFile({ safelistMap, sourceFile });
+  const cssInput = await convertSafelistMapToTailwindCss(safelistMap);
+  const cssOutput = await $`echo ${cssInput} | tailwindcss -i -`.text();
+  // combine all css safelist values into a single string
+  const cssValues = Array.from(safelistMap.values()).flatMap((item) => [
+    ...item,
+  ]);
+  const cssSafelist = cssValues.join('\n');
+
+  const sourceFileImports = sourceFile.getImportDeclarations();
+  sourceFileImports.map((item) => item.remove());
+  const jsOutput = sourceFile
+    .getEmitOutput()
+    .getOutputFiles()[0]
+    .getText()
+    .replaceAll('_a.jsx', 'jsx');
+
+  return {
+    cssInput,
+    cssOutput,
+    cssSafelist,
+    jsOutput,
+  };
+}
 
 export async function PUT(request: Request) {
   const formData = await request.formData();
   const id = formData.get('id') as string;
   const jsInput = formData.get('jsInput') as string;
-
-  const transformedData = await transformPlaygroundCode({
+  const { cssInput, cssOutput, cssSafelist, jsOutput } = await transformJs({
     id,
     jsInput,
   });
 
   await db.doc(`playgrounds/${id}`).update({
     jsInput,
-    ...transformedData,
+    cssInput,
+    cssOutput,
+    cssSafelist,
+    jsOutput,
   });
+
   return NextResponse.json({ message: 'Playground updated' });
+}
+
+export async function POST(request: Request) {
+  const id = crypto.randomUUID();
+  const { jsInput } = await request.json();
+  const { cssInput, cssOutput, cssSafelist, jsOutput } = await transformJs({
+    id,
+    jsInput,
+  });
+
+  await db.doc(`playgrounds/${id}`).set({
+    jsInput,
+    cssInput,
+    cssOutput,
+    cssSafelist,
+    jsOutput,
+  });
+
+  return NextResponse.json({ id });
 }
--- apps/web-demo/src/app/page.tsx
+++ apps/web-demo/src/app/page.tsx
@@ -1,7 +1,38 @@
 import { redirect } from 'next/navigation';
-import { createPlayground } from '@/actions/create-playground';
+
+const exampleCode = `
+import { VStack, Text, Icon } from 'katcn';
+import { getStyles } from 'katcn/getStyles';
+
+function Example() {
+  const customStyles = getStyles({
+    borderWidth: '2',
+    borderColor: 'warning',
+    bg: 'accent'
+  });
+
+  return (
+    <VStack bg="alert">
+      <VStack width="1/2" bg="accent">
+        <Text color="on-accent" variant="display1" className={customStyles}>
+          something
+        </Text>
+        <Icon name="addFile" size="4" />
+      </VStack>
+    </VStack>
+  )
+}
+`;
 
 export default async function Home() {
-  const { id } = await createPlayground();
+  const response = await fetch('http://localhost:3000/api/playground', {
+    method: 'POST',
+    body: JSON.stringify({
+      jsInput: exampleCode,
+    }),
+  });
+
+  const { id } = await response.json();
+
   return redirect(`/playground/${id}`);
 }
